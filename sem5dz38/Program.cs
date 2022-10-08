08d4a347baa704710159c38da4c1b973bc4b001d
evgenii <evgenii.lyabov@mail.ru>
2022-10-08 23:34:32 +0300

    add sem5dz38

---
+++ sem5dz38/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+Console.Clear();
+double[] mas = new double[6];
+Random r = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = r.Next(-10, 10);
+}
+Console.WriteLine("Входной массив:" + "[ " + String.Join(", ", mas) + " ]");
+
+Array.Sort(mas);
+double maxValue = mas[mas.Length - 1];
+Console.WriteLine("Максимальный элемент массива:" + "[ " + String.Join(", ", maxValue) + " ]");
+double minValue = mas.Min();
+Console.WriteLine("Минимальный элемент массива:" + "[ " + String.Join(", ", minValue) + " ]");
+double result = maxValue - minValue;
+Console.WriteLine("Разница между max и min:" + "[ " + String.Join(", ", result) + " ]");
